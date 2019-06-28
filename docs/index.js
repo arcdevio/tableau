3e0a8259eb388440b4b4945b5f55881d8e327ab6
Alexander Elias <alex.steven.elias@gmail.com>
2019-06-28 15:39:49 -0700

    simplified code

--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,7 @@
 
-
 const url = 'https://public.tableau.com/views/RegionalSampleWorkbook/Storms';
 
-const o = new tableau.Viz(document.querySelector('.one'), url,{
+const vizOne = new tableau.Viz(document.querySelector('.one'), url, {
     hideTabs: true,
     onFirstInteractive: function () {
         document.querySelector('.one-status').textContent = 'One Ready';
@@ -10,10 +9,22 @@ const o = new tableau.Viz(document.querySelector('.one'), url,{
     }
 });
 
-const t = new tableau.Viz(document.querySelector('.two'), url, {
+const vizTwo = new tableau.Viz(document.querySelector('.two'), url, {
     hideTabs: true,
     onFirstInteractive: function () {
         document.querySelector('.two-status').textContent = 'Two Ready';
         document.querySelector('[onclick="showTwo()"]').disabled = false;
     }
 });
+
+const showOne = function () {
+    document.querySelector('.status').textContent = 'Showing ONE Iframe';
+    document.querySelector('.one').classList.add('active');
+    document.querySelector('.two').classList.remove('active');
+};
+
+const showTwo = function () {
+    document.querySelector('.status').textContent = 'Showing TWO Iframe';
+    document.querySelector('.two').classList.add('active');
+    document.querySelector('.one').classList.remove('active');
+};
